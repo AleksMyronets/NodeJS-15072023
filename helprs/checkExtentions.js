88a99ce55ffc0078575a22d951836a98144f7c7d
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-15 11:26:59 +0300

    Update checkExtentions.js

--- helprs/checkExtentions.js
+++ helprs/checkExtentions.js
@@ -1,3 +1,14 @@
-const checkExtention = (fileName) => {
-    const EXTENTIONS = ['js', 'html', 'css', 'json', 'txt']
-}
+const checkExtension = (fileName) => {
+  const EXTENSIONS = ["js", "json", "html", "css", "txt"];
+
+  const fileParts = fileName.split(".");
+  if (fileParts.length > 1) {
+    const extension = fileParts[fileParts.length - 1];
+    if (EXTENSIONS.includes(extension)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
