78cfd68474f98b49bac09e7a2073e3147016c651
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-15 11:23:02 +0300

    Create files.js

---
+++ files.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises');
+
+const path = require('path');
+
+const chalk = require('chalk');
+
+const helper = require('./helprs/dataValidator')
+
+const creatFile = async (fileName, content) => {
+    const file = {
+        fileName,
+        content
+    }
+    const result = dataValidator(file);
+    // console.log(result.error.details[0]);
+    if (result.error) {
+        const test = chalk.red(`Please specify ${result.error.details[0].path} parameter`)
+        console.log(test);
+        return;
+    }
+
+}
+
+module.exports = {
+    creatFile,
+}
