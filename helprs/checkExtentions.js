fd0e83da14d9821e38cf01db2c4e7c3527ed04f5
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-07-15 12:49:38 +0300

    Update checkExtentions.js

--- helprs/checkExtentions.js
+++ helprs/checkExtentions.js
@@ -1,7 +1,7 @@
 const checkExtension = (fileName) => {
     const EXTENSIONS = ["js", "json", "html", "css", "txt"];
     const index = fileName.lastIndexOf('.');
-    const extension = slice(index + 1);
+    const extension = fileName.slice(index + 1);
     // const finded = EXTENSIONS.find(() => { extension });
 
     const result = EXTENSIONS.some(el => el === extension)
